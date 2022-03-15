46dc599b2e3a9d1bbbf1eda0f49a02c5d9f0ca5a
Max Desiatov <max@desiatov.com>
2022-03-15 13:12:04 +0000

    Fix `#if !os(WASI)` condition

--- Sources/XCTest/Private/XCTestCase.TearDownBlocksState.swift
+++ Sources/XCTest/Private/XCTestCase.TearDownBlocksState.swift
@@ -22,7 +22,7 @@ extension XCTestCase {
         @available(macOS 12.0, *)
         func appendAsync(_ block: @Sendable @escaping () async throws -> Void) {
             self.append {
-                #if os(WASI)
+                #if !os(WASI)
                 try awaitUsingExpectation { try await block() }
                 #else
                 Task {
